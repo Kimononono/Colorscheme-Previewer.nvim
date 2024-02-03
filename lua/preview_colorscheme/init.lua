879dc3cc3053196887f4bf7a349b3f8ecb0361da
Jordan Mandel <jordan.mandel@live.com>
2024-02-02 22:18:19 -0500

    removed functionality I didn't want

--- lua/preview_colorscheme/init.lua
+++ lua/preview_colorscheme/init.lua
@@ -1,24 +1,6 @@
 local M = {}
 
-local function save_colorscheme()
-	local colorscheme = vim.g.colors_name
-	local file = io.open(vim.fn.stdpath('config') .. '/colorscheme.vim', 'w')
-	if file then
-		file:write('colorscheme ' .. colorscheme)
-		file:close()
-	end
-end
-
-local function load_colorscheme()
-	local file = io.open(vim.fn.stdpath('config') .. '/colorscheme.vim', 'r')
-	if file then
-		local colorscheme = file:read("*a")
-		file:close()
-		vim.cmd(colorscheme)
-	end
-end
-
-local function _preview_colorscheme()
+local function preview_colorscheme()
 	local cmdline = vim.fn.getcmdline()
 	local cmdpos = vim.fn.getcmdpos()
 	local cmd_parts = vim.split(cmdline, ' ')
@@ -39,17 +21,10 @@ M.setup = function()
 	local color_preview = vim.api.nvim_create_augroup("aesthetic_settings", { clear = true })
 
 	vim.api.nvim_create_autocmd("CmdlineChanged", {
-		callback = _preview_colorscheme,
+		callback = preview_colorscheme,
 		group = color_preview
 	})
 
-	vim.api.nvim_create_autocmd("ColorScheme", {
-		callback = save_colorscheme,
-		group = color_preview
-	})
-
-
-	load_colorscheme()
 end
 
 return M
